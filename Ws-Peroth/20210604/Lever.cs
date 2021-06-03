62b5dfa204c19e89f454501994cc267a75df0393
Ws-Peroth <a01033056809@gmail.com>
2021-06-03 23:27:24 +0900

    code review
    2021/06/03 코드리뷰

---
+++ Ws-Peroth/20210604/Lever.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : Obstacle
+{
+    public SpriteRenderer spriteRenderer;
+
+    Color defaultColor = new Color(0.5f, 1f, 0.5f, 1f);
+    Color activeColor = Color.black;
+
+    private void Start() => 
+        spriteRenderer.color = defaultColor;
+
+    public void SendSignal()
+    {
+        object[] sendData = new object[] { 
+            new ObstacleObject[] { ObstacleObject.door }, // Broadcasting Targets
+            GetKey(), // Key
+            !GetStatus()    // Status
+        };
+        MapManager.mapMaker.SendMessage(sendData);
+    }
+
+    override public void SetStatus(bool status)
+    {
+        base.SetStatus(status);
+        spriteRenderer.color = status ? activeColor : defaultColor;
+    }
+}
