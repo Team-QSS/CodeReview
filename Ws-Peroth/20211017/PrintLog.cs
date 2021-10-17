d5bfbb542fdc5cc3ddfeebe58001569d8eadfe05
Ws-Peroth <a01033056809@gmail.com>
2021-10-17 22:32:16 +0900

    이원석 211017
    코드리뷰 요청
    - FirebaseLoginManager.cs 의 33 ~ 39 Line의 문제점 수정
    - 이전에 추천해주신 로그인 관련 클래스 구성의 구현에서 현재 구현한 것이 말하신 방법인지 모르겠음
    - 코드의 가독성 향상
    
    클래스 관계
    LoginManager : FirebaseLoginManager와 OAuthloginManager을 합쳐서 관리함
    
    FirebaseLoginManager : Firebase관련 로그인 기능 구현
    
    OAuthLoginManager : OAuth관련 로그인 기능 구현
    
    TryLogin : 로그인을 시도하는 클래스
    
    PrintLog : 모바일 화면에서 Text UI로 로그를 찍어주는 역할

---
+++ Ws-Peroth/20211017/PrintLog.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PrintLog : Singleton<PrintLog>
+{
+    [SerializeField]
+    private Text _logText;
+
+    public string LogString 
+    {
+        get => _logText.text;
+        set => _logText.text = $"{value}\n";
+    }
+
+
+}
